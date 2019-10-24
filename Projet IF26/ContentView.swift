ae3cfb386be183c86d49afc7a792c6e82d67aa34
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-10-24 23:53:45 +0200

    Initial commit

---
+++ Projet IF26/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Projet IF26
+//
+//  Created by Hugo Locurcio on 24/10/2019.
+//  Copyright © 2019 UTT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
