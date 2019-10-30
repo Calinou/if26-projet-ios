95303921871d0718e3ed41b5f301a52b36aeff68
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-10-30 20:07:45 +0100

    Improve display of transaction rows
    The amount will now be colored depending on the transaction type
    (income, expense or transfer).

--- Projet IF26/TransactionRow.swift
+++ Projet IF26/TransactionRow.swift
@@ -14,24 +14,44 @@ struct TransactionRow: View {
     var body: some View {
         HStack {
             Text(transaction.category.rawValue)
-            Spacer()
-            VStack {
+                .foregroundColor(.gray)
+                .frame(width: 80.0)
+            
+            VStack(alignment: .leading) {
                 Text(transaction.contents)
+                    // Increase the line height slightly
+                    .frame(height: 23.0)
                 Text(transaction.account.rawValue)
+                    .font(.footnote)
+                    .foregroundColor(.gray)
             }
+            .padding(.leading)
+            
             Spacer()
-            Text(String(format: "%.2f €", Double(transaction.amount) / 100))
+            
+            Text(
+                String(
+                    // Add a "+" symbol for income to make it easier to discern.
+                    // Negative numbers are already prefixed with a "-", no need to do it manually.
+                    format: "\(!transaction.isTransfer && transaction.amount > 0 ? "+" : "")%.2f €",
+                    Double(transaction.amount) / 100
+                )
+            ).foregroundColor(
+                transaction.isTransfer || transaction.amount == 0 ? .gray :
+                transaction.amount > 0 ? .blue :
+                .red
+            )
         }
         .padding()
-        
     }
 }
 
 struct TransactionRow_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            TransactionRow(transaction: transactionData[0])
-            TransactionRow(transaction: transactionData[1])
+            TransactionRow(transaction: transactionData[0]) // Income
+            TransactionRow(transaction: transactionData[1]) // Expense
+            TransactionRow(transaction: transactionData[3]) // Transfer
         }
         .previewLayout(.fixed(width: 360, height: 65))
     }
