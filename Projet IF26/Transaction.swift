00d80c9b2123018ed98c8f45caac78fd29291aa5
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-11-08 21:22:49 +0100

    Add documentation for Transaction properties

--- Projet IF26/Transaction.swift
+++ Projet IF26/Transaction.swift
@@ -10,13 +10,37 @@ import SwiftUI
 import Foundation
 
 struct Transaction: Hashable, Codable, Identifiable {
+    /// The transaction's unique identifier
     var id: Int
+    
+    /// The amount of the transaction (in cents)
     var amount: Int
+    
+    var amountString: String {
+        get {
+            String(amount)
+        }
+        set(newAmount) {
+            amount = Int(newAmount) ?? 0
+        }
+    }
+    
+    /// The transaction date (stored as an UNIX timestamp)
     var date: Int
+    
+    /// The account the transaction was made with
     var account: Account
+    
+    /// The category the transaction belongs to
     var category: Category
+    
+    /// An one-line description of the transaction (shown on the list of transactions)
     var contents: String
+    
+    /// A multi-line description of the transaction (not shown on the list of transactions)
     var notes: String
+    
+    /// If `true`, the transaction is a transfer from an account to another
     var isTransfer: Bool
     
     enum Account: String, CaseIterable, Codable, Hashable {
