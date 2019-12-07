ff3138694848971274c278a6887c9c4d478b4239
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-12-07 20:18:50 +0100

    Remove an unnecessary cast to String

--- Projet IF26/TransactionRow.swift
+++ Projet IF26/TransactionRow.swift
@@ -29,7 +29,7 @@ struct TransactionRow: View {
 
             Spacer()
 
-            Text(String(transaction.formattedAmount))
+            Text(transaction.formattedAmount)
                 .foregroundColor(transaction.amountColor)
         }
         .padding()
